906511402776b3ca1f55e7724f05f91daf565f4a
Gautam Sharma <gautamthestark1@gmail.com>
2021-12-17 01:48:05 +0530

    added counter page

---
+++ app/routes/counter.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Form, redirect, useLoaderData } from 'remix';
+
+export const loader = async () => {
+  const response = await fetch('https://worker.lms-oto.workers.dev/');
+  const result = await response.json();
+  return result;
+};
+
+export async function action() {
+  fetch('https://worker.lms-oto.workers.dev/increment', {
+    method: 'post'
+  });
+  return redirect('/');
+}
+
+const Index = () => {
+  const data = useLoaderData();
+  return (
+    <div>
+      <pre>{JSON.stringify(data)}</pre>
+      <Form method='post'>
+        <button type='submit'>INCREMENT</button>
+      </Form>
+    </div>
+  );
+};
+
+export default Index;
